0353ae8dfed1435eab0304bd6a2af0886763b890
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-07 12:03:00 +0200

    Add drag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,15 @@ import { useEffect, useRef, type CSSProperties } from "react";
 
 export default function Home() {
   const scrollContainer = useRef<HTMLDivElement | null>(null);
-  
+  const isDragging = useRef<boolean>(false);
+  const dragData = useRef<{ startX: number; scrollLeft: number; lastTime: number; lastX: number; velocity: number }>({
+    startX: 0,
+    scrollLeft: 0,
+    lastTime: 0,
+    lastX: 0,
+    velocity: 0,
+  });
+
   const handleWheel = (e: WheelEvent) => {
     if (scrollContainer.current !== null) {
       if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
@@ -16,11 +24,70 @@ export default function Home() {
     }
   };
 
-    useEffect(() => {
+  const handleMouseDown = (e: MouseEvent) => {
+    isDragging.current = true;
+    dragData.current.startX = e.clientX;
+    if (scrollContainer.current) {
+      dragData.current.scrollLeft = scrollContainer.current.scrollLeft;
+    }
+    dragData.current.lastTime = Date.now();
+    dragData.current.lastX = e.clientX;
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isDragging.current) return;
+  
+    if (scrollContainer.current) {
+      const deltaX = e.clientX - dragData.current.startX;
+      scrollContainer.current.scrollLeft = dragData.current.scrollLeft - deltaX;
+  
+      const currentTime = Date.now();
+      const timeDelta = currentTime - dragData.current.lastTime;
+      const positionDelta = dragData.current.lastX - e.clientX;
+  
+      dragData.current.velocity = positionDelta / timeDelta;
+      dragData.current.lastTime = currentTime;
+      dragData.current.lastX = e.clientX;
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (!isDragging.current) return;
+
+    isDragging.current = false;
+    applyInertia();
+  };
+
+  const applyInertia = () => {
+    if (!scrollContainer.current) return;
+
+    const friction = 0.96;
+
+    const animate = () => {
+      if (!scrollContainer.current) return;
+
+      scrollContainer.current.scrollLeft += dragData.current.velocity;
+      dragData.current.velocity *= friction;
+
+      if (Math.abs(dragData.current.velocity) > 0.5) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
     window.addEventListener('wheel', handleWheel, { passive: false });
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
 
     return () => {
       window.removeEventListener('wheel', handleWheel);
+      window.removeEventListener('mousedown', handleMouseDown);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
     };
   }, []);
 
