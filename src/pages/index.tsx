1c6b5604d7d2a00860507a6140b6206dbb191181
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-08 12:01:00 +0200

    Update drag to framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,78 @@
+import { motion, useMotionValue } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useRef, type CSSProperties } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const scrollContainer = useRef<HTMLDivElement | null>(null);
-  const isDragging = useRef<boolean>(false);
-  const dragData = useRef<{ startX: number; scrollLeft: number; lastTime: number; lastX: number; velocity: number }>({
-    startX: 0,
-    scrollLeft: 0,
-    lastTime: 0,
-    lastX: 0,
-    velocity: 0,
-  });
+  const [windowWidth, setWindowWidth] = useState(0);
 
-  const handleWheel = (e: WheelEvent) => {
-    if (scrollContainer.current !== null) {
-      if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
-        scrollContainer.current.scrollLeft += e.deltaY * 0.5;
-        e.preventDefault();
-      } else {
-        scrollContainer.current.scrollLeft += e.deltaX * 0.5;
-      }
-    }
-  };
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
 
-  const handleMouseDown = (e: MouseEvent) => {
-    isDragging.current = true;
-    dragData.current.startX = e.clientX;
-    if (scrollContainer.current) {
-      dragData.current.scrollLeft = scrollContainer.current.scrollLeft;
-    }
-    dragData.current.lastTime = Date.now();
-    dragData.current.lastX = e.clientX;
-  };
-
-  const handleMouseMove = (e: MouseEvent) => {
-    if (!isDragging.current) return;
-  
-    if (scrollContainer.current) {
-      const deltaX = e.clientX - dragData.current.startX;
-      scrollContainer.current.scrollLeft = dragData.current.scrollLeft - deltaX;
-  
-      const currentTime = Date.now();
-      const timeDelta = currentTime - dragData.current.lastTime;
-      const positionDelta = dragData.current.lastX - e.clientX;
-  
-      dragData.current.velocity = positionDelta / timeDelta;
-      dragData.current.lastTime = currentTime;
-      dragData.current.lastX = e.clientX;
-    }
-  };
-
-  const handleMouseUp = () => {
-    if (!isDragging.current) return;
-
-    isDragging.current = false;
-    applyInertia();
-  };
-
-  const applyInertia = () => {
-    if (!scrollContainer.current) return;
-
-    const friction = 0.96;
-
-    const animate = () => {
-      if (!scrollContainer.current) return;
-
-      scrollContainer.current.scrollLeft += dragData.current.velocity;
-      dragData.current.velocity *= friction;
-
-      if (Math.abs(dragData.current.velocity) > 0.5) {
-        requestAnimationFrame(animate);
-      }
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
     };
 
-    requestAnimationFrame(animate);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const imageArray = [
+    "/assets/2461288.jpg",
+    "/assets/2461292.jpg",
+    "/assets/2461294.jpg",
+    "/assets/2461302.jpg",
+    "/assets/2461309.jpg",
+    "/assets/2461313.jpg",
+    "/assets/8302965.jpg",
+  ];
+
+  const width = 300;
+  const gap = 40;
+  const scrollX = useMotionValue(0);
+  const paddingValue = `0 calc(50vw - ${width / 2}px)`;
+
+  const [, paddingBlock, paddingInline] = paddingValue.match(
+    /^([\d\s.-]+)\s+([\w\s().+-]+)$/
+  );
+
+  function calcToPx(expression:any) {
+    const regex = /calc\(([\d.]+)vw\s*-\s*([\d.]+)px\)/;
+    const [, vwValue, pixelValue] = expression.match(regex);
+
+    const pixels =
+      parseFloat(vwValue) * (windowWidth / 100) - parseFloat(pixelValue);
+    return pixels;
+  }
+
+
+  function getWidth(items: any) {
+    const totalWidth = items.length * width;
+    const totalGap = (items.length - 1) * gap;
+    const totalScroll = totalWidth + totalGap + calcToPx(paddingInline) * 2;
+    return totalScroll;
+  }
+
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleWheel = (e: WheelEvent) => {
+    if (containerRef.current !== null) {
+      if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
+        containerRef.current.scrollLeft += e.deltaY * 0.5;
+        e.preventDefault();
+      } else {
+        containerRef.current.scrollLeft += e.deltaX * 0.5;
+      }
+    }
   };
 
   useEffect(() => {
-    window.addEventListener('wheel', handleWheel, { passive: false });
-    window.addEventListener('mousedown', handleMouseDown);
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener("wheel", handleWheel, { passive: false });
 
     return () => {
-      window.removeEventListener('wheel', handleWheel);
-      window.removeEventListener('mousedown', handleMouseDown);
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener("wheel", handleWheel);
     };
   }, []);
 
@@ -99,66 +84,36 @@ export default function Home() {
       </Head>
       <main className="relative min-h-screen">
         <div
-          ref={scrollContainer}
-          className="absolute bottom-1/2 flex w-full [&::-webkit-scrollbar]:hidden translate-y-1/2 gap-10 overflow-x-auto px-[--px]"
-          style={{ "--px": "calc(50vw - 12.5rem)" } as CSSProperties}
+          ref={containerRef}
+          className="w-full overflow-x-auto [&::-webkit-scrollbar]:hidden"
         >
-          <Image
-            src="/assets/2461288.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/2461292.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/2461294.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/2461302.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/2461309.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/2461313.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
-          <Image
-            src="/assets/8302965.jpg"
-            alt=""
-            className="h-[20rem] w-[40rem] object-cover"
-            width={1920}
-            height={600}
-            draggable="false"
-          />
+          <motion.div
+            drag="x"
+            dragConstraints={{
+              left: -getWidth(imageArray) + windowWidth,
+              right: 0,
+            }}
+            style={{
+              width: getWidth(imageArray),
+              x: scrollX,
+              gap: gap,
+              padding: paddingValue,
+            }}
+            className="flex min-h-screen items-center"
+          >
+            {imageArray.map((element, index) => (
+              <Image
+                key={index}
+                src={element}
+                alt=""
+                draggable="false"
+                className="w-[4 0rem] h-[20rem] select-none object-cover"
+                style={{ width: width }}
+                width={1920}
+                height={600}
+              />
+            ))}
+          </motion.div>
         </div>
         <div className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-white before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-white after:content-['']" />
       </main>
