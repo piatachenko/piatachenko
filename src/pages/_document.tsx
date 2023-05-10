cd976c6f9a16c7e469d2250ebb6ad6275aa2227d
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-10 12:00:00 +0200

    Test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-black text-zinc-100 touch-pan-x">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,113 +1,90 @@
-import { motion, useMotionValue } from "framer-motion";
-import Head from "next/head";
-import Image from "next/image";
-import { CSSProperties, useEffect, useRef, useState } from "react";
-
-const imageArray = [
-  "/assets/2461288.jpg",
-  "/assets/2461292.jpg",
-  "/assets/2461294.jpg",
-  "/assets/2461302.jpg",
-  "/assets/2461309.jpg",
-  "/assets/2461313.jpg",
-  "/assets/8302965.jpg",
-];
+import { animate, motion, useMotionValue } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const [windowWidth, setWindowWidth] = useState(0);
+  const ref = useRef<HTMLUListElement>(null);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [trackMouse, setTrackMouse] = useState(false);
+  const [animationComplete, setAnimationComplete] = useState(true);
+  const [items, setItems] = useState<number[]>([]);
 
   useEffect(() => {
-    setWindowWidth(window.innerWidth);
-
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    const arr = new Array(10).fill(undefined).map((val, idx) => idx);
+    setItems(arr);
   }, []);
 
-  const handleWheel = (e: WheelEvent) => {
-    if (containerRef.current !== null) {
-      if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
-        containerRef.current.scrollLeft += e.deltaY * 0.5;
-        e.preventDefault();
-      } else {
-        containerRef.current.scrollLeft += e.deltaX * 0.5;
-      }
+  const x = useMotionValue(0);
+
+  const handleMouseMove = (e: React.PointerEvent<HTMLUListElement>) => {
+    if (!ref.current) return;
+    if (!trackMouse) return;
+
+    setAnimationComplete(false);
+
+    const xVal = e.pageX - ref.current.offsetLeft;
+    const walk = (xVal - startX) * 2; //scroll-fast
+
+    const controls = animate(x, scrollLeft - walk, {
+      type: "tween",
+      ease: "easeOut",
+      duration: 0.5,
+      onUpdate: (val) => {
+        if (!ref.current) return;
+        ref.current.scrollLeft = val;
+      },
+      onComplete: () => {
+        setAnimationComplete(true);
+      },
+      onStop: () => {
+        setAnimationComplete(true);
+      },
+    });
+    return controls.stop;
+  };
+
+  const handleMouseDown = (e: React.PointerEvent<HTMLUListElement>) => {
+    if (!ref.current) return;
+
+    setTrackMouse(true);
+
+    const startX = e.pageX - ref.current.offsetLeft;
+    setStartX(startX);
+
+    const scrollLeft = ref.current.scrollLeft;
+    setScrollLeft(scrollLeft);
+  };
+
+  const handleMouseLeave = () => {
+    setTrackMouse(false);
+  };
+
+  const handleMouseUp = () => {
+    setTrackMouse(false);
+  };
+
+  const handleScroll = () => {
+    if (!ref.current) return;
+
+    if (animationComplete) {
+      x.set(ref.current.scrollLeft);
     }
   };
 
-  useEffect(() => {
-    window.addEventListener("wheel", handleWheel, { passive: false });
-
-    return () => {
-      window.removeEventListener("wheel", handleWheel);
-    };
-  }, []);
-
-  const scrollX = useMotionValue(0);
-  const [width, gap] = [300, 40];
-  const [paddingInlineVwValue, paddingInlinePxValue] = [50, width / 2];
-  const paddingInline = `calc(${paddingInlineVwValue}vw - ${paddingInlinePxValue}px)`;
-
-  function calcPaddingInlineToPx() {
-    return (paddingInlineVwValue * windowWidth) / 100 - paddingInlinePxValue;
-  }
-
-  function getWidth(items: string[]) {
-    const totalWidth = items.length * width;
-    const totalGap = (items.length - 1) * gap;
-    const totalScroll = totalWidth + totalGap + calcPaddingInlineToPx() * 2;
-    return totalScroll;
-  }
-
   return (
     <>
-      <Head>
-        <title>IP: Work</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="relative min-h-screen">
-        <div
-          ref={containerRef}
-          className="w-full overflow-x-auto [&::-webkit-scrollbar]:hidden"
-        >
-          <motion.div
-            drag="x"
-            dragConstraints={{
-              left: -getWidth(imageArray) + windowWidth,
-              right: 0,
-            }}
-            style={
-              {
-                width: getWidth(imageArray),
-                x: scrollX,
-                gap: gap,
-                "padding-inline": paddingInline,
-              } as CSSProperties
-            }
-            className="flex min-h-screen items-center"
-          >
-            {imageArray.map((element, index) => (
-              <Image
-                key={index}
-                src={element}
-                alt=""
-                draggable="false"
-                className="h-[20rem] select-none object-cover"
-                style={{ width: width }}
-                width={1920}
-                height={600}
-              />
-            ))}
-          </motion.div>
-        </div>
-        <div className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-white before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-white after:content-['']" />
-      </main>
+      <motion.ul
+        ref={ref}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+        onScroll={handleScroll}
+      >
+        {items.map((idx) => (
+          <motion.li key={idx}></motion.li>
+        ))}
+      </motion.ul>
     </>
   );
 }
