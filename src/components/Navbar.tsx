5d21a22b9da9ee813a46ac12966d5491c1054407
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-20 12:00:00 +0200

    Move to scroller motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ interface NavbarProps {
 export default function Navbar({ page }: NavbarProps) {
   return (
     <>
-      <nav className="absolute right-1/2 top-9 z-10 translate-x-1/2">
+      <nav className="fixed right-1/2 top-9 z-10 translate-x-1/2">
         <div className="flex gap-10">
           {pages.map((element, id) => (
             <Link
@@ -19,7 +19,7 @@ export default function Navbar({ page }: NavbarProps) {
                   ? "pointer-events-none"
                   : "ease-[cubic-bezier(0.25, 0.46, 0.45, 0.94)] opacity-30 transition-opacity duration-300 hover:opacity-100"
               }
-              href={element === 'Work' ? '/' : `/${element.toLowerCase()}`}
+              href={element === "Work" ? "/" : `/${element.toLowerCase()}`}
             >
               {element}
             </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { animate, motion, useMotionValue } from "framer-motion";
+import { motion } from "framer-motion";
 import Head from "next/head";
-import { useEffect, useRef, useState, type CSSProperties } from "react";
+import { useEffect, type CSSProperties } from "react";
+import { ScrollerMotion } from "scroller-motion";
 import MainLayout from "~/layouts/MainLayout";
 
 const imageArray = [
@@ -14,125 +15,53 @@ const imageArray = [
 ];
 
 export default function Home() {
-  const handleWheel = (e: WheelEvent) => {
-    if (ref.current !== null) {
-      if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
-        ref.current.scrollLeft += e.deltaY * 0.3; // Change the multiplier value
-        e.preventDefault();
-      } else {
-        ref.current.scrollLeft += e.deltaX * 0.3; // Change the multiplier value
-      }
-    }
-  };
+  // const [windowWidth, setWindowWidth] = useState(0);
+
+  // useEffect(() => {
+  //   function updateWindowWidth() {
+  //     setWindowWidth(window.innerWidth);
+  //   }
+
+  //   updateWindowWidth();
+  //   window.addEventListener("resize", updateWindowWidth);
+
+  //   return () => {
+  //     window.removeEventListener("resize", updateWindowWidth);
+  //   };
+  // }, []);
+
+  // const [dragConstraints, setDragConstraints] = useState({ left: 0, right: 0 });
+
+  // useEffect(() => {
+  //   function onScroll() {
+  //     const scrollWidth = document.documentElement.scrollWidth;
+  //     const scrollLeft = document.documentElement.scrollLeft;
+  //     const left =
+  //       (-(scrollWidth / scale - windowWidth) *
+  //         (scrollWidth - windowWidth - scrollLeft)) /
+  //       (scrollWidth - windowWidth);
+  //     const right =
+  //       ((scrollWidth / scale - windowWidth) * scrollLeft) /
+  //       (scrollWidth - windowWidth);
+  //     setDragConstraints({ left, right });
+  //   }
+  //   document.documentElement.addEventListener("mouseover", onScroll);
+  //   return () =>
+  //     document.documentElement.removeEventListener("mouseover", onScroll);
+  // }, [windowWidth]);
+
+  const scale = 2;
 
   useEffect(() => {
-    window.addEventListener("wheel", handleWheel, { passive: false });
-
-    return () => {
-      window.removeEventListener("wheel", handleWheel);
-    };
+    function onWheel(e: WheelEvent) {
+      if (!e.deltaY) {
+        return;
+      }
+      document.documentElement.scrollLeft += e.deltaY + e.deltaX;
+    }
+    window.addEventListener("wheel", onWheel);
+    return () => window.removeEventListener("wheel", onWheel);
   }, []);
-
-  const ref = useRef<HTMLUListElement>(null);
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-  const [trackMouse, setTrackMouse] = useState(false);
-  const [animationComplete, setAnimationComplete] = useState(true);
-  const [lastDragVelocity, setLastDragVelocity] = useState(0);
-  const [bgXPositions, setBgXPositions] = useState(() => {
-    return imageArray.map((_, index) => {
-      const parallaxSpeed = 0.02;
-      const elementWidth = 360;
-      const elementScrollPercentage = (-index * elementWidth) / elementWidth;
-      const parallaxOffset = elementScrollPercentage * parallaxSpeed * 100;
-
-      return 50 + parallaxOffset;
-    });
-  });
-
-  const x = useMotionValue(0);
-
-  const handleMouseMove = (e: React.PointerEvent<HTMLUListElement>) => {
-    if (!ref.current) return;
-    if (!trackMouse) return;
-
-    const xVal = e.pageX - ref.current.offsetLeft;
-    const walk = (xVal - startX) * 1; //scroll-fast
-    const newScrollLeft = scrollLeft - walk;
-
-    const velocity = newScrollLeft - ref.current.scrollLeft;
-    setLastDragVelocity(velocity);
-
-    ref.current.scrollLeft = newScrollLeft;
-  };
-
-  const handleMouseDown = (e: React.PointerEvent<HTMLUListElement>) => {
-    if (!ref.current) return;
-
-    setTrackMouse(true);
-
-    const startX = e.pageX - ref.current.offsetLeft;
-    setStartX(startX);
-
-    const scrollLeft = ref.current.scrollLeft;
-    setScrollLeft(scrollLeft);
-  };
-
-  const handleMouseLeave = () => {
-    setTrackMouse(false);
-  };
-
-  const handleMouseUp = (e: React.PointerEvent<HTMLUListElement>) => {
-    setTrackMouse(false);
-
-    if (!ref.current) return;
-
-    const endX = e.pageX - ref.current.offsetLeft;
-
-    // Check if the mouse has actually moved
-    if (Math.abs(startX - endX) > 2) {
-      const targetScrollLeft = ref.current.scrollLeft + lastDragVelocity * 10; // Adjust the multiplier for desired momentum
-
-      void animate(ref.current.scrollLeft, targetScrollLeft, {
-        type: "spring",
-        stiffness: 250,
-        damping: 30,
-        velocity: lastDragVelocity,
-        onUpdate: (val) => {
-          if (!ref.current) return;
-          ref.current.scrollLeft = val;
-        },
-        onComplete: () => {
-          setAnimationComplete(true);
-        },
-        onStop: () => {
-          setAnimationComplete(true);
-        },
-      });
-    }
-  };
-
-  const handleScroll = () => {
-    if (!ref.current) return;
-
-    if (animationComplete) {
-      x.set(ref.current.scrollLeft);
-    }
-
-    const scrollAmount = ref.current.scrollLeft;
-    const parallaxSpeed = 0.02;
-
-    const newBgXPositions = bgXPositions.map((_, index) => {
-      const elementWidth = 360;
-      const elementScrollPercentage =
-        (scrollAmount - index * elementWidth) / elementWidth;
-      const parallaxOffset = elementScrollPercentage * parallaxSpeed * 100;
-
-      return 50 + parallaxOffset;
-    });
-
-    setBgXPositions(newBgXPositions);
-  };
 
   return (
     <>
@@ -141,31 +70,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout page="Work">
-        <main className="relative min-h-screen">
-          <motion.ul
-            ref={ref}
-            onMouseMove={handleMouseMove}
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-            onMouseLeave={handleMouseLeave}
-            onScroll={handleScroll}
-            className="absolute bottom-1/2 flex w-full translate-y-1/2 gap-10 overflow-x-auto px-[--px] [&::-webkit-scrollbar]:hidden"
-            style={{ "--px": "calc(50vw - 10rem)" } as CSSProperties}
-          >
-            {imageArray.map((element, index) => (
-              <motion.li
-                key={index}
-                className="h-[30rem] w-[20rem] shrink-0 select-none bg-[image:--bg-image] bg-cover bg-[var(--bg-x-position)_center]"
-                style={
-                  {
-                    "--bg-image": `url('${element}')`,
-                    "--bg-x-position": `${bgXPositions[index] ?? 50}%`,
-                  } as CSSProperties
-                }
-              />
-            ))}
-          </motion.ul>
-          <div className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-white before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-white after:content-['']" />
+        <main>
+          <ScrollerMotion scale={scale}>
+            <motion.ul
+              className="flex min-h-screen items-center pl-[--px]"
+              style={{ "--px": "calc(50vw - 10rem)" } as CSSProperties}
+            >
+              {imageArray.map((element, index) => (
+                <motion.li
+                  key={index}
+                  className="h-[30rem] w-[20rem] shrink-0 select-none bg-[image:--bg-image] bg-cover bg-[var(--bg-x-position)_center] [&:not(:first-of-type)]:ml-10"
+                  style={
+                    {
+                      "--bg-image": `url('${element}')`,
+                    } as CSSProperties
+                  }
+                />
+              ))}
+              <div className="h-1 w-[--px] shrink-0 bg-transparent" />
+            </motion.ul>
+          </ScrollerMotion>
+          <div className="fixed bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-white before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-white after:content-['']" />
         </main>
       </MainLayout>
     </>
