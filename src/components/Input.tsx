1f74f79e89fa535370d6a01d19df1734b10bd859
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-25 12:03:00 +0200

    Add textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,16 +2,18 @@ import { useRef, type CSSProperties } from "react";
 
 interface InputProps {
   placeholder?: string;
+  type?: string;
   customClass?: string;
 }
 
-export default function Input({ placeholder, customClass }: InputProps) {
+export default function Input({ placeholder, type, customClass }: InputProps) {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const divRef = useRef<HTMLDivElement | null>(null);
   const duration = 700;
 
   function onChange() {
-    const input = inputRef.current;
+    const input = inputRef.current ?? textareaRef.current;
     const div = divRef.current;
     if (input && div) {
       if (input.value) {
@@ -32,13 +34,24 @@ export default function Input({ placeholder, customClass }: InputProps) {
         style={{ "--duration": `${duration}ms` } as CSSProperties}
         className="relative mb-10 block pb-2 text-4xl after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-0 after:bg-white after:content-['']"
       >
-        <input
-          ref={inputRef}
-          type={"text"}
-          placeholder={placeholder}
-          onChange={onChange}
-          className={`w-full bg-transparent outline-none ${customClass}`}
-        />
+        {type === "textarea" ? (
+          <textarea
+            ref={textareaRef}
+            placeholder={placeholder}
+            onChange={onChange}
+            className={`h-60 w-full resize-none bg-transparent outline-none ${customClass}`}
+            contentEditable
+          />
+          
+        ) : (
+          <input
+            ref={inputRef}
+            type={"text"}
+            placeholder={placeholder}
+            onChange={onChange}
+            className={`w-full bg-transparent outline-none ${customClass}`}
+          />
+        )}
       </div>
     </>
   );
