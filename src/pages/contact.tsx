423956e6b919888684411664eea676e531f09b5c
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-25 12:04:00 +0200

    Add form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,29 @@
 import Head from "next/head";
+import Input from "~/components/Input";
 import MainLayout from "~/layouts/MainLayout";
 
 export default function Contact() {
   return (
     <>
       <Head>
-        <title>Ivan Piatachenko</title>
+        <title>IP: Contact</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout page="Contact">
-        <main></main>
+        <main>
+          <div className="flex min-h-screen items-center px-[10vw]">
+            <form action="" className="block">
+              <Input placeholder="Your name" customClass="" />
+              <Input placeholder="you@email.com" customClass="" type="email" />
+              <Input
+                placeholder="How can i help?"
+                customClass=""
+                type="textarea"
+              />
+              <input type="submit" value="Send" />
+            </form>
+          </div>
+        </main>
       </MainLayout>
     </>
   );
