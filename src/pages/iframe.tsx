1280ba8964d35640890b743ac9797f5df29cedb7
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-26 12:04:00 +0200

    Fix current item position

--- src/pages/iframe.tsx
+++ src/pages/iframe.tsx
@@ -76,9 +76,11 @@ export default function Iframe() {
     <>
       <main className="iframe">
         <div
-          className="fixed z-10 bottom-[--bottom] right-1/2 flex translate-x-1/2 items-center justify-center gap-1 mix-blend-difference"
+          className="fixed bottom-[--bottom] right-1/2 z-10 flex translate-x-1/2 items-center justify-center gap-1 mix-blend-difference"
           style={
-            { "--bottom": `calc((100vh - var(--h)) / 5)` } as CSSProperties
+            {
+              "--bottom": "max(calc((100vh - var(--h)) / 5), 1rem)",
+            } as CSSProperties
           }
         >
           <div className="h-[1.5em] overflow-hidden">
