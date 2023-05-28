d26fbb348d67c433891a53629324962e42b7c9e7
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-28 12:01:00 +0200

    Add contact responsive

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,16 +11,20 @@ export default function Contact() {
       </Head>
       <MainLayout page="Contact">
         <main>
-          <div className="flex min-h-screen items-center px-[10vw]">
-            <form action="" className="block">
-              <Input placeholder="Your name" customClass="" />
-              <Input placeholder="you@email.com" customClass="" type="email" />
-              <Input
-                placeholder="How can i help?"
-                customClass=""
-                type="textarea"
-              />
-              <input type="submit" value="Send" />
+          <div className="flex min-h-screen items-center px-[2rem] py-[10rem] sm:px-[3rem] md:px-[7%] xl:px-[10%] 2xl:px-[13%]">
+            <form action="" className="w-full">
+              <div className="gap-10 md:flex xl:gap-20 2xl:gap-24">
+                <Input placeholder="Your name" />
+                <Input placeholder="your@email.com" type="email" />
+              </div>
+              <Input placeholder="How can I help?" type="textarea" />
+              <div className="flex justify-center mix-blend-difference">
+                <input
+                  type="submit"
+                  value="Send"
+                  className="-mt-5 translate-y-5 p-4 text-center text-3xl"
+                />
+              </div>
             </form>
           </div>
         </main>
