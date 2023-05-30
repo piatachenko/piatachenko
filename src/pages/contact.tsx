8be6ec61395b1ff44c80ebfbfab979b04db4307c
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-30 12:02:00 +0200

    Upd sizes

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
+import { type FormEvent } from "react";
 import Input from "~/components/Input";
 import MainLayout from "~/layouts/MainLayout";
 
 export default function Contact() {
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <Head>
@@ -11,15 +16,19 @@ export default function Contact() {
       </Head>
       <MainLayout page="Contact">
         <main>
-          <div className="flex min-h-screen items-center px-[2rem] py-[10rem] sm:px-[3rem] md:px-[7%] xl:px-[10%] 2xl:px-[13%]">
-            <form action="" className="w-full">
+          <div className="flex min-h-screen items-center px-[2rem] py-24 sm:px-[3rem] md:px-[7%] xl:px-[10%] 2xl:px-[13%]">
+            <form
+              onSubmit={handleSubmit}
+              className="w-full"
+              style={{ paddingTop: "min(2.5rem, 3vh)" }}
+            >
               <div className="gap-10 md:flex xl:gap-20 2xl:gap-24">
                 <Input placeholder="Your name" />
                 <Input placeholder="your@email.com" type="email" />
               </div>
               <Input placeholder="How can I help?" type="textarea" />
               <div className="flex flex-col justify-center mix-blend-difference">
-                <button className="bg-zinc-100 p-4 text-4xl text-zinc-950 outline-none ring-zinc-500 ring-offset-2 ring-offset-zinc-950 hover:cursor-pointer focus:ring-2">
+                <button className="bg-zinc-100 p-2 text-4xl text-zinc-950 outline-none ring-zinc-500 ring-offset-2 ring-offset-zinc-950 hover:cursor-pointer focus:ring-2">
                   Send
                 </button>
                 <div className="mx-auto mt-10 text-sm">
