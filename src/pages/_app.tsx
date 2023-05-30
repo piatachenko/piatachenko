2102456e906bac33a89cb9c62d94558357947366
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-30 12:00:00 +0200

    Add scroll listener in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,66 @@
-import { type AppType } from "next/app";
-import { useRouter } from "next/router";
-
+import { type AppProps } from "next/app";
+import { type Router } from "next/router";
+import {
+  createContext,
+  useEffect,
+  useRef,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  const router = useRouter();
+interface ScrollPositionContextData {
+  scrollPosition: number;
+  setScrollPosition: Dispatch<SetStateAction<number>>;
+}
+
+export const ScrollPositionContext = createContext<
+  ScrollPositionContextData | undefined
+>(undefined);
+
+export default function MyApp({
+  Component,
+  pageProps,
+  router,
+}: AppProps & { router: Router }) {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const value = { scrollPosition, setScrollPosition };
+
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    const iframe = iframeRef.current?.contentWindow;
+
+    if (iframe) {
+      const handleScroll = () => {
+        setScrollPosition(iframe.document.documentElement.scrollLeft);
+      };
+
+      iframe.addEventListener("scroll", handleScroll);
+
+      return () => {
+        iframe.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [iframeRef]);
+
+  useEffect(() => {
+    setScrollPosition(0);
+  }, [router.pathname]);
 
   return (
     <>
-      <iframe
-        src="/iframe"
-        className={
-          router.pathname === "/" ? "absolute h-full w-full" : "hidden"
-        }
-      />
-      <Component {...pageProps} />
+      <ScrollPositionContext.Provider value={value}>
+        <iframe
+          ref={iframeRef}
+          src="/iframe"
+          className={
+            router.pathname === "/" ? "absolute h-full w-full" : "hidden"
+          }
+        />
+        <Component {...pageProps} />
+      </ScrollPositionContext.Provider>
     </>
   );
-};
-
-export default MyApp;
+}
