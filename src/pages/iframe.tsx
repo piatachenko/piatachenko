239a4433e213304d84c8aeace3b16f748030e127
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-05-30 12:01:00 +0200

    Move carousel current item to index page

--- src/pages/iframe.tsx
+++ src/pages/iframe.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ScrollerMotion } from "scroller-motion";
 
 export const imageArray = [
@@ -11,57 +11,7 @@ export const imageArray = [
 ];
 
 export default function Iframe() {
-  // const [dragConstraints, setDragConstraints] = useState({ left: 0, right: 0 });
-  // useEffect(() => {
-  //   function onScroll() {
-  //     const scrollWidth = document.documentElement.scrollWidth;
-  //     const scrollLeft = document.documentElement.scrollLeft;
-  //     const left =
-  //       (-(scrollWidth / scale - windowWidth) *
-  //         (scrollWidth - windowWidth - scrollLeft)) /
-  //       (scrollWidth - windowWidth);
-  //     const right =
-  //       ((scrollWidth / scale - windowWidth) * scrollLeft) /
-  //       (scrollWidth - windowWidth);
-  //     setDragConstraints({ left, right });
-  //   }
-  //   document.documentElement.addEventListener("mouseover", onScroll);
-  //   return () =>
-  //     document.documentElement.removeEventListener("mouseover", onScroll);
-  // }, [windowWidth]);
-
-  const [currentItem, setCurrentItem] = useState(1);
-  const [scrollLeft, setScrollLeft] = useState(0);
-  const [windowWidth, setWindowWidth] = useState(0);
-
-  useEffect(() => {
-    function updateWindowWidth() {
-      setWindowWidth(window.innerWidth);
-    }
-    updateWindowWidth();
-    window.addEventListener("resize", updateWindowWidth);
-    return () => {
-      window.removeEventListener("resize", updateWindowWidth);
-    };
-  }, []);
-
-  useEffect(() => {
-    function onScroll() {
-      setScrollLeft(document.documentElement.scrollLeft);
-    }
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  useEffect(() => {
-    const imageWidth =
-      (document.documentElement.scrollWidth - windowWidth) /
-      (imageArray.length - 1);
-    const newCurrentItem = Math.floor(scrollLeft / imageWidth + 1.5);
-    setCurrentItem(newCurrentItem);
-  }, [scrollLeft, windowWidth]);
-
-  useEffect(() => {
+    useEffect(() => {
     function onWheel(e: WheelEvent) {
       if (!e.deltaY) {
         return;
@@ -75,28 +25,6 @@ export default function Iframe() {
   return (
     <>
       <main className="iframe">
-        <div
-          className="fixed right-1/2 z-10 flex translate-x-1/2 translate-y-1/2 items-center justify-center gap-1 mix-blend-difference"
-          style={{
-            bottom: "max(calc((100vh - var(--h)) / 4), 2rem)",
-          }}
-        >
-          <div className="h-[1.5em] overflow-hidden">
-            {imageArray.map((_, index) => (
-              <div
-                key={index}
-                className="transition-all duration-[.8s] ease-out"
-                style={{
-                  transform: `translateY(-${(currentItem - 1) * 100}%)`,
-                }}
-              >
-                {index + 1}
-              </div>
-            ))}
-          </div>
-          <div>-</div>
-          <div>{imageArray.length}</div>
-        </div>
         <ScrollerMotion scale={1.25}>
           <motion.ul
             className="flex min-h-screen items-center"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
 import Head from "next/head";
+import { useContext, useEffect, useState } from "react";
 import MainLayout from "~/layouts/MainLayout";
+import { ScrollPositionContext } from "./_app";
+import { imageArray } from "./iframe";
 
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [currentItem, setCurrentItem] = useState(0);
+  const context = useContext(ScrollPositionContext);
+
+  if (!context) {
+    throw new Error(
+      "ChildComponent must be used within a ScrollPositionProvider"
+    );
+  }
+
+  const { scrollPosition } = context;
+
+  useEffect(() => {
+    function updateWindowWidth() {
+      setWindowWidth(window.innerWidth);
+    }
+    updateWindowWidth();
+    window.addEventListener("resize", updateWindowWidth);
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
+  useEffect(() => {
+    const scrollWidth =
+      document.querySelector("iframe")?.contentWindow?.document.documentElement
+        .scrollWidth;
+    if (scrollWidth) {
+      const imageWidth = (scrollWidth - windowWidth) / (imageArray.length - 1);
+      const newCurrentItem = Math.floor(scrollPosition / imageWidth + 1.5);
+      setCurrentItem(newCurrentItem);
+    }
+  }, [scrollPosition, windowWidth]);
+
   return (
     <>
       <Head>
@@ -9,6 +46,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout page={"Work"}>
+        <div
+          className="fixed right-1/2 z-10 flex translate-x-1/2 translate-y-1/2 items-center justify-center gap-1 mix-blend-difference"
+          style={{
+            bottom: "max(calc((100vh - var(--h)) / 4), 2rem)",
+          }}
+        >
+          <div className="h-[1.5em] overflow-hidden">
+            {imageArray.map((_, index) => (
+              <div
+                key={index}
+                className="transition-all duration-[.8s] ease-out"
+                style={{
+                  transform: `translateY(-${(currentItem - 1) * 100}%)`,
+                }}
+              >
+                {index + 1}
+              </div>
+            ))}
+          </div>
+          <div>-</div>
+          <div>{imageArray.length}</div>
+        </div>
         <div className="fixed bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 mix-blend-difference before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-white before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-white after:content-['']" />
       </MainLayout>
     </>
