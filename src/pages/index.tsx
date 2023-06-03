500ce5bb237ab9c57787504b43b66b2ce37b4052
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-06-03 12:04:00 +0200

    Upd main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useContext, useEffect, useState } from "react";
 import MainLayout from "~/layouts/MainLayout";
-import { imageArray } from "./iframe";
+import { projects } from "./iframe";
 import { ScrollPositionContext } from "./_app";
 
 export default function Home() {
@@ -33,7 +33,7 @@ export default function Home() {
       document.querySelector("iframe")?.contentWindow?.document.documentElement
         .scrollWidth;
     if (scrollWidth) {
-      const imageWidth = (scrollWidth - windowWidth) / (imageArray.length - 1);
+      const imageWidth = (scrollWidth - windowWidth) / (projects.length - 1);
       const newCurrentItem = Math.floor(scrollPosition / imageWidth + 1.5);
       setCurrentItem(newCurrentItem);
     }
@@ -53,7 +53,7 @@ export default function Home() {
           }}
         >
           <div className="h-[1.5em] overflow-hidden">
-            {imageArray.map((_, index) => (
+            {projects.map((_, index) => (
               <div
                 key={index}
                 className="transition-all duration-[.8s] ease-out"
@@ -66,9 +66,9 @@ export default function Home() {
             ))}
           </div>
           <div>-</div>
-          <div>{imageArray.length}</div>
+          <div>{projects.length}</div>
         </div>
-        <div className="fixed bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 mix-blend-difference before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-zinc-100 before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-zinc-100 after:content-['']" />
+        <div className="pointer-events-none fixed bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 mix-blend-difference before:absolute before:bottom-1/2 before:right-1/2 before:block before:h-[.11rem] before:w-7 before:translate-x-1/2 before:translate-y-1/2 before:rotate-90 before:bg-zinc-100 before:content-[''] after:absolute after:bottom-1/2 after:right-1/2 after:block after:h-[.11rem] after:w-7 after:translate-x-1/2 after:translate-y-1/2 after:bg-zinc-100 after:content-['']" />
       </MainLayout>
     </>
   );
