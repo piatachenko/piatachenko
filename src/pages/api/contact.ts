7a31f3dd36148410ba1692e2bd895d5bd25006a9
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-06-04 12:02:00 +0200

    Fix vercel

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
+import { type NextApiRequest, type NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
+type RequestBody = {
+  name: string;
+  email: string;
+  message: string;
+};
+
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { name, email, message } = req.body;
+  const { name, email, message } = req.body as RequestBody;
 
   const formData = await prisma.formData.create({
     data: {
